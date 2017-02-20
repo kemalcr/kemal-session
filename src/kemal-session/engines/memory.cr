bfe2431af9aba7ab5a39a123ba36a4cee7e2c619
Jason Landry <jasonl99@gmail.com>
2017-02-20 12:00:17 -0500

    fixed missing end

--- src/kemal-session/engines/memory.cr
+++ src/kemal-session/engines/memory.cr
@@ -62,8 +62,9 @@ class Session
     def run_gc
       before = (Time.now - Session.config.timeout.as(Time::Span)).epoch_ms
       @store.each do |id, entry|
-      last_access_at = Int64.from_json(entry, root: "last_access_at")
-      Session.timeout(id) if last_access_at < before
+        last_access_at = Int64.from_json(entry, root: "last_access_at")
+        Session.timeout(id) if last_access_at < before
+      end
       sleep Session.config.gc_interval
     end
 

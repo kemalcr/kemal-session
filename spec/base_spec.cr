cecfe3a6e9b9972f755ec4aca142721850efb798
Rimas Silkaitis <rimas@heroku.com>
2017-04-06 14:28:57 -0700

    whatever

--- spec/base_spec.cr
+++ spec/base_spec.cr
@@ -118,6 +118,7 @@ describe "Session" do
       session.object("obj", UserTestDeserialization.new(1_i64))
       s = Session.get(SESSION_ID)
       expect_raises Exception, "calling from_json" do
+        puts "it works"
         s.as(Session).object("obj").as(UserTestDeserialization)
       end
     end
